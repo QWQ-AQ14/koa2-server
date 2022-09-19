bcd869e61432d3dc915c0443b69a2988b7e70143
xlq <1135506045@qq.com>
2022-09-19 21:16:20 +0800

    feat:登录接口

--- src/controller/user.js
+++ src/controller/user.js
@@ -17,7 +17,7 @@ const {
   changeInfoFailInfo,
   changePasswordFailInfo
 } = require('../model/ErrorInfo')
-
+const doCrypto = require('../utils/cryp')
 /**
  * 用户名是否存在
  * @param {string} userName 用户名
@@ -59,7 +59,28 @@ async function register({ userName, password, gender }) {
     return new ErrorModel(registerFailInfo)
   }
 }
+/**
+ * 登录
+ * @param {Object} ctx koa2 ctx
+ * @param {string} userName 用户名
+ * @param {string} password 密码
+ */
+async function login(ctx, userName, password) {
+  // 获取用户信息
+  const userInfo = await getUserInfo(userName, doCrypto(password))
+  if (!userInfo) {
+    // 登录失败
+    return new ErrorModel(loginFailInfo)
+  }
+
+  // 登录成功
+  if (ctx.session.userInfo == null) {
+    ctx.session.userInfo = userInfo
+  }
+  return new SuccessModel()
+}
 module.exports = {
   isExist,
-  register
+  register,
+  login
 }
