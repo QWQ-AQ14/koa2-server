eb1c459a5ef4b0917508cc69596ddc30a5020143
xlq <1135506045@qq.com>
2022-09-19 11:37:18 +0800

    feat:密码加密

--- src/controller/user.js
+++ src/controller/user.js
@@ -17,6 +17,7 @@ const {
   changeInfoFailInfo,
   changePasswordFailInfo
 } = require('../model/ErrorInfo')
+
 /**
  * 用户名是否存在
  * @param {string} userName 用户名
@@ -33,7 +34,32 @@ async function isExist(userName) {
     return new ErrorModel(registerUserNameNotExistInfo)
   }
 }
+/**
+ * 注册
+ * @param {string} userName 用户名
+ * @param {string} password 密码
+ * @param {number} gender 性别（1 男，2 女，3 保密）
+ */
+async function register({ userName, password, gender }) {
+  const userInfo = await getUserInfo(userName)
+  if (userInfo) {
+    // 用户名已存在
+    return new ErrorModel(registerUserNameExistInfo)
+  }
+
+  try {
+    await createUser({
+      userName,
+      password: doCrypto(password),
+      gender
+    })
+    return new SuccessModel()
+  } catch (ex) {
+    console.error(ex.message, ex.stack)
+    return new ErrorModel(registerFailInfo)
+  }
+}
 module.exports = {
   isExist,
-
+  register
 }
